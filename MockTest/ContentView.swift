97bde224c3eba290a47002ef2f95416ff9da8289
akash1711 <nsakashu@gmail.com>
2021-07-03 16:43:11 +0530

    Initial Commit

---
+++ MockTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MockTest
+//
+//  Created by Akash NS on 03/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MockTest/MockTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MockTestApp.swift
+//  MockTest
+//
+//  Created by Akash NS on 03/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MockTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
